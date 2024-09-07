87e00ec48979632f9319262c98a27d8724ee3aa8
Kazu <hlyanw@yahoo.com>
2024-09-07 07:29:39 +0630

    Refactored diamond code

--- diamond.py
+++ diamond.py
@@ -1,67 +1,42 @@
 
-def pattern(letter, inverted=False, common_row=True, value=1) -> list:
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def pattern(letter, pieces, common_row=True) -> list:
     # the index is included up to that letter
-    if not common_row:
-        at_letter = alphabet.index(letter)
-    else:
-        at_letter = alphabet.index(letter) + 1
-
-    gap = 1
-    # odd sequence of gap
-    # gaps = [odd for odd in range(1, at_letter + 3, 2)]
-    # print(gaps)
-
-    # calculate the diamond's width & height (not nessary)
-    # width = ((alphabet.index(letter) + 1) * 2) - 1
-
-    # checking mode
-    # for upright
-    if not inverted:
-        # needed character
-        character = alphabet[:at_letter]
-        # number of num_space in each side (left and right)
-        num_space = at_letter - value
-        # # start of an odd sequence
-        # gap = 1
-    else:
-    # for inverted
-        start = alphabet.index(letter)
-        num_space = 0
-
-        # for outputing the last common row
-        # if common_row:
-        #     character = alphabet[start::-1]
-        # else:
-        #     character =  alphabet[start-1::-1]        
-
+    at_letter = pieces.index(letter) + 1
+    # gaps go with odd sequence
+    gap = 1   
+    # needed character
+    character = pieces[1:at_letter]
+    # number of num_space in each side (left and right)
+    num_space = at_letter - 1
     
-    result = []
+    # first row is not fit into my logic so I kicked out.
+    first_item = row = " " * num_space + pieces[0] + " " * num_space
+    result = [first_item]
     # top half including the middle row
-    for _, chr in enumerate(character):
-        # print(chr)
-        
-        # first row is not fit into my theory so I kicked out.
-        if chr == 'A':
-            row = " " * num_space + chr + " " * num_space
-        else:
-            row = " " * num_space + chr + " " * gap + chr + " " * num_space
-            # gap increases as an odd sequence 
-            gap += 2
-        
-        # if inverted:
-        #     # num_space is increased
-        #     num_space += 1
-        # else:
-            # num_space is decreased
+    for chr in character:
+        # we need to decrease early bcz of the first row
         num_space -= 1
+        row = " " * num_space + chr + " " * gap + chr + " " * num_space
+        # gap increases as an odd sequence 
+        gap += 2
         
-
         result.append(row)
-    
+    # the last row will include or not
+    if not common_row and len(result) > 1:
+        result.pop()
+
     return result
 
 def rows(letter):
-    return pattern(letter, common_row=False, value=0) + list(reversed(pattern(letter)))
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if not (letter in alphabet):
+        return f"Unallowed character\nAllowed character: {alphabet}"
+    if letter == alphabet[0]:
+        return [alphabet[0]]
+    return pattern(letter, pieces=alphabet, common_row=False) + list(reversed(pattern(letter, pieces=alphabet)))
 
+if __name__ == "__main__":
+    x = rows("E")
+    for i in range(len(x)):
+        print(x[i])
 
